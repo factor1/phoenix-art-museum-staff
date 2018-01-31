f1e20630bfaa9974481abde62bf98e97afd21b2d
Melanie <mlpatt23@gmail.com>
2018-01-31 11:38:00 -0700

    Adjusted js file for ie11 class and applied browser specific styles

--- docent-directory/assets/js/staff.js
+++ docent-directory/assets/js/staff.js
@@ -1,6 +1,16 @@
-var isSafari = navigator.vendor && navigator.vendor.indexOf('Apple') > -1 &&
-               navigator.userAgent && !navigator.userAgent.match('CriOS');
+window.onload = function(){
+  var isSafari = navigator.vendor && navigator.vendor.indexOf('Apple') > -1 &&
+                 navigator.userAgent && !navigator.userAgent.match('CriOS');
 
-if(isSafari) {
-  document.body.classList.add("safari");
+  if(isSafari) {
+    document.body.classList.add("safari");
+  }
+
+  var isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
+
+
+  if(isIE11) {
+    //console.log('ie11');
+    document.body.classList.add("ie11");
+  }
 }
