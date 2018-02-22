b17aa95de5cad00187c5b8dface96e78e46f660d
Melanie <mlpatt23@gmail.com>
2018-02-22 08:55:02 -0700

    Styled export button

--- docent-directory/templates/directory/list.php
+++ docent-directory/templates/directory/list.php
@@ -44,7 +44,9 @@
 	<?php if($is_admin): ?>
 		<div class="s-row single-staff">
 			<div class="s-col-12 col--flex-column">
-				<a href="<?php echo $links['export']; ?>" class="staff-export">Export Results As CSV</a>
+				<div>
+					<a href="<?php echo $links['export']; ?>" class="staff-export">Export Results As CSV</a>
+				</div>
 			</div>
 		</div>
 	<?php endif; ?>
